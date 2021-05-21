f39d7becaa3ff1610d257a923ae29411288bdb5c
Kate <71548252+Techi77@users.noreply.github.com>
2021-05-21 22:20:28 +0300

    fix: code bug

--- app/src/main/java/com/example/mytestapp/MainActivity.kt
+++ app/src/main/java/com/example/mytestapp/MainActivity.kt
@@ -1,24 +1,27 @@
 package com.example.mytestapp
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var callButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         callButton = findViewById(R.id.my_awesome_button)
         callButton.setOnClickListener {
-            if(isCallPermissionGranted())
-            {
+            if(isCallPermissionGranted()){
                 callEmergencyNumber()
             }else {
                 requestCallPermissions()
@@ -32,10 +35,15 @@ class MainActivity : AppCompatActivity() {
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), REQUEST_CODE)
     }
 
+    @SuppressLint("QueryPermissionsNeeded")
     private fun callEmergencyNumber(){
-        val callUri= Uri.parse("tell://911")
+        val callUri= Uri.parse("tel://911")
         val callIntent = Intent(Intent.ACTION_CALL, callUri)
-        startActivity(callIntent)
+        if (callIntent.resolveActivity(packageManager) != null) {
+            startActivity(callIntent)
+        } else {
+            Log.d("DEBUG", "No Intent available to handle action")
+        }
     }
     private companion object{
         private const val REQUEST_CODE = 100
